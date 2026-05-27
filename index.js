ae9801be0805ed027422a8427dfd059487ceb295
quotex90 <te877124@gmail.com>
2026-05-27 17:28:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Quotex USA Running");
+  res.send("Quotex USA Running Successfully");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
